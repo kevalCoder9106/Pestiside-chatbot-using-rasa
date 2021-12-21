e4bf67efe1652a1ba157cc7391c1ada80bf7f1a0
kevalCoder9106 <kevalcoder@gmail.com>
2021-12-21 20:00:09 +0530

    Adding files

---
+++ actions/actions.py
@@ -0,0 +1,38 @@
+# This files contains your custom actions which can be used to run
+# custom Python code.
+
+# See this guide on how to implement these action:
+# https://rasa.com/docs/rasa/custom-actions
+
+from typing import Any, Text, Dict, List
+
+from rasa_sdk import Action, Tracker
+from rasa_sdk.executor import CollectingDispatcher
+
+
+class CropName(Action):
+
+    def name(self) -> Text:
+        return "utter_crop_name"
+
+    def run(self, dispatcher: CollectingDispatcher,
+            tracker: Tracker,
+            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+
+        dispatcher.utter_message(text=Text)
+
+        return []
+
+
+class ActionHelloWorld(Action):
+
+    def name(self) -> Text:
+        return "action_hello_world"
+
+    def run(self, dispatcher: CollectingDispatcher,
+            tracker: Tracker,
+            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+
+        dispatcher.utter_message(text="Hello World!")
+
+        return []
